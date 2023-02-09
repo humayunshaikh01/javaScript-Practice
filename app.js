7bc1bdd2d829a3e3c122ffeeacfb28548960e058
humayunshaikh01 <shaikhhumayun99@gmail.com>
2023-02-10 01:42:11 +0500

    js practice

---
+++ app.js
@@ -0,0 +1,68 @@
+// alert ("hello");
+
+// var fullName = "Humayun Shaikh";
+// var age = 24;
+// var contactNumber = "03201232583";
+// var email = "shaikhhumayun@gmail.com";
+// var isActive = true;
+// var result = undefined;
+// var relationStatus = null;
+
+// console.log(fullName);
+// console.log(age);
+// console.log(contactNumber);
+// console.log(email);
+// console.log(isActive);
+// console.log(result);
+// console.log(relationStatus);
+
+// alert (fullName);
+// alert (age);
+// alert (contactNumber);
+// alert (email);
+// alert (isActive);
+// alert (result);
+// alert (relationStatus);
+
+// var firstName = "Humayun";
+// var lastName = "Shaikh"
+// var fullName = firstName + " " + lastName;
+// console.log(fullName);
+
+// var a = 45;
+// var b = 50;
+// var mul = a * b;
+// console.log(mul);
+
+// var value = 30 + 30 + "30";
+// console.log(value);
+
+// var reminder =  43 % 3;
+// console.log(reminder);
+
+// var value = 15 + 18 - 22 * (30 - 17);
+
+// = 15 + 18 - 22 * (13);
+// = 15 + 18 - 286;
+// = 33 - 286;
+// =  -253;
+
+// console.log(value);
+
+var a =  15;
+var value1 = a++ + ++a  -  --a;
+
+// 15 + 17 - 16
+
+// 16
+
+console.log(value1)
+
+           // 16 // 17 // 18 //17 //16 //16 //16  //17
+var value2 = ++a + ++a - --a + a-- + a - a - a++ - --a;
+
+             // 17 + 18 - 17 + 17 + 16 - 16 - 16 - 16
+
+             // 3
+
+             console.log(value2)
