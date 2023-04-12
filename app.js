03a7196170e129f8906d4cd293943a0acbb1f42c
humayunshaikh01 <shaikhhumayun99@gmail.com>
2023-04-13 00:23:05 +0500

    more practicing repush

--- app.js
+++ app.js
@@ -49,20 +49,514 @@
 
 // console.log(value);
 
-var a =  15;
-var value1 = a++ + ++a  -  --a;
+// var a =  15;
+// var value1 = a++ + ++a  -  --a;
 
-// 15 + 17 - 16
+// // 15 + 17 - 16
 
-// 16
+// // 16
 
-console.log(value1)
+// console.log(value1)
 
-           // 16 // 17 // 18 //17 //16 //16 //16  //17
-var value2 = ++a + ++a - --a + a-- + a - a - a++ - --a;
+//            // 16 // 17 // 18 //17 //16 //16 //16  //17
+// var value2 = ++a + ++a - --a + a-- + a - a - a++ - --a;
 
-             // 17 + 18 - 17 + 17 + 16 - 16 - 16 - 16
+//              // 17 + 18 - 17 + 17 + 16 - 16 - 16 - 16
 
-             // 3
+//              // 3
 
-             console.log(value2)
+//              console.log(value2)
+
+// Array Practice
+
+// var subjects = ["Enlish", "Urdu", "Maths", "Sidhi"];
+// console.log("Before Splice", subjects);
+
+// subjects.unshift("Chemistry");
+// console.log("New Array", subjects);
+
+// subjects.shift();
+// console.log("New Array", subjects);
+
+// subjects.pop(1, "Physics");
+// console.log("New Array", subjects);
+
+// subjects.push("Chemistry");
+// console.log("New Array", subjects);
+
+// var subjectsCopy = subjects.slice(1 , 3);
+// console.log("Copy Array", subjectsCopy);
+
+// subjects.splice(-1, 0, "Physics");
+// console.log("After Splice", subjects);
+
+// object base
+// "JavaScript, often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS."
+
+// var stR = // object base
+// "JavaScript, often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS."
+
+// for (var i = 0; i < stR.length; i++){
+//     document.write(i)
+// }
+
+
+
+// for Quiz
+
+// practice for alert
+
+// alert ("Hello user! how can i help You")
+
+// alert("Error! Please enter a valid Password.")
+
+// alert("Welcome to JS Land... \n Happy Coding!")
+
+// alert("Welcome to JS Land...")
+
+// alert("Happy Coding! \n Prevent this page from creating additional dialogs")
+
+
+
+
+// practice for var
+
+// var userName;
+
+// var myName = "Humayun Shaikh"
+// alert(myName)
+
+// var userName = "Humayun";
+// var userAge = 24;
+// var userQual = "Certified Mobile Application Development"
+
+// alert(userName);
+// alert(userAge);
+// alert(userQual);
+
+
+// var Pizza = "Pizza \n Pizz \n Piz \n Pi \n P"
+// alert(Pizza);
+
+// var email = "Example@example.com";
+// alert("My email address is " + email);
+
+// var book = "A smarter way to learn JavaScript";
+// alert("I'm trying to learn from the Book " + book)
+
+// document.write("Yah! I can write HTML content through JavaScript")
+
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+
+// var myAge = 24;
+// alert("I'm " + myAge + " years old");
+
+// var visitCount = 14;
+// alert("You have visit this web " + visitCount + " times")
+
+// var birthYear = 1998;
+// document.write("My Birth Year is " + birthYear + "<br />" + "Data type of my Variable is Number")
+
+// var visitorName = "John Doe";
+// var productTitle = "T-Shirt";
+// var Quantity = 5;
+// document.write(visitorName + " ordered " + Quantity + " " + productTitle + "(s) on xyz Clothing Store");
+
+// var myName = "Humayun Shaikh", myGender = "male", myAge = 24;
+
+// legal variable names
+// var myName;
+// var _myName;
+// var $name;
+// var name1;
+// var my_Name;
+
+// illegal variable names
+// var my-Name;
+// var 1name;
+// var my Name;
+// var my.Name;
+// var /myName
+
+// document.write("<h1>“Rules for naming JS variables”</h1>");
+// document.write("Variable names can only contains 'numbers, $, _. ex. $my_1stVariable <br />");
+// document.write("Variables must begin with a names, $, _. ex.$name, _name or name <br />");
+// document.write("Varibales names are case sensitive <br />");
+// document.write("Variable names should not be JS Keywords");
+
+// Chapter #5 Math Experssions
+
+// var num1 = 5;
+// var num2 = 10;
+// var sum = num1 + num2;
+// document.write("Sum of " + num1 + " and " + num2 + " is " + sum);
+
+// var num1 = 5;
+// var num2 = 10;
+// var minus = num1 - num2;
+// document.write("Minus of " + num1 + " and " + num2 + " is " + minus);
+
+// var num1 = 5;
+// var num2 = 10;
+// var multi = num1 * num2;
+// document.write("Multiplication of " + num1 + " and " + num2 + " is " + multi);
+
+
+// var num1 = 5;
+// var num2 = 10;
+// var divid = num1 / num2;
+// document.write("division of " + num1 + " and " + num2 + " is " + divid);
+
+
+// var num1 = 10;
+// var num2 = 5;
+// var modulus = num1 % num2;
+// document.write("Modulus of " + num1 + " and " + num2 + " is " + modulus);
+
+// var num;
+
+// var num = undefined;
+// document.write("<br/> Value after declaration is " + num);
+
+// var num = 5;
+// document.write("<br/> initialize the value is " + num);
+
+// num++;
+// document.write("<br/> Value after increment is " + num);
+
+// num +=7;
+// document.write("<br/> value after addition is " + num);
+
+// num--;
+// document.write("<br/> value after decrement is " + num)
+
+// var remainder = num % 3;
+// document.write("<br/> value after modulus is " + remainder)
+
+// var ticketPrice = 600;
+// var totalAmount = ticketPrice * 5
+// document.write("Total Cost to buy 5 Tickets to a movie is " + totalAmount + " PKR");
+
+
+// for(var i = 1; i <= 10; i++){
+//     document.write("3 x " + i + " = " + i*3 + "<br />")
+// }
+
+
+// if else practice
+
+// var askUser = +prompt("How old are you?")
+
+// if(askUser >= 18 && askUser <= 45){
+//     alert("You are Allowed!")
+// }
+
+// else if(askUser > 45 && askUser <= 100){
+//     alert("Sorry! You are over age.")
+// }else{
+//     alert("You are not Allowed")
+// }
+
+
+// var now = new Date().toDateString();
+// console.log(now)
+
+// var now = new Date("8 march 1998")
+// console.log(now)
+
+// var now = new Date()
+// var day = now.getMonth()
+// console.log(day)
+
+// var dayNames = ["Sunday", "Monday", "Tuesday"]
+// var day = new Date().getDay()
+// console.log(dayNames[2])
+
+// var time = new Date("March 08 1998").getTime();
+// var currMiliSec = new Date().getTime()
+
+// // console.log(time);
+// // console.log(currMiliSec);
+
+// var diff = currMiliSec - time
+
+// // console.log(diff)
+
+// var totalYear = Math.round(diff / (1000 * 60 * 60 * 24 * 365));
+// console.log("I'm " + totalYear + " Years old")
+
+
+
+
+// var time = new Date("30 January 2000").getTime()
+// var currentMilisec = new Date().getTime()
+
+// console.log(time)
+// console.log(currentMilisec);
+
+// var diffYear = currentMilisec - time
+// console.log(diffYear);
+
+// var totalYear = Math.round(diffYear /(1000 * 60 * 60 * 24 * 365))
+// console.log(totalYear)
+
+
+// var time = new Date("30 January 1970").getTime()
+// var currentMilisec = new Date().getTime()
+
+// console.log(time)
+// console.log(currentMilisec)
+
+// var diff = currentMilisec - time
+// console.log(diff)
+
+// var totalYear = Math.round(diff / (1000 * 60 * 60 * 24 * 365))
+// console.log(totalYear)
+
+// var d = new Date()
+// d.setMonth(2)
+// console.log(d)
+
+// function foo(){
+//     console.log("Hellow")
+//     console.log("user")
+// }
+
+// foo();
+
+// function sum(){
+//     var num1 = +prompt("Enter num1")
+//     var num2 = +prompt("Enter num2")
+//     console.log("sum of two numbers = " + (num1 + num2))
+
+// }
+
+// sum()
+
+
+// function greetuser (userName){
+//     console.log("user name is " + userName)
+//     console.log("Hellow " + userName)
+
+// }
+// greetuser("Humayun")
+// greetuser("Bilal")
+
+
+
+// function greet(greetUser){
+//     var greetUser = prompt("Enter Name")
+//     console.log("Hello " + greetUser)
+// }
+// greet()
+
+
+// function addition(value1 , value2){
+//     var value1 = +prompt("Enter num1")
+//     var value2 = +prompt("Enter num2")
+
+//     console.log(value1 + " value1")
+//     console.log(value2 + " value2")
+//     console.log(value1 - value2 + " (Maths rules Applying)")
+// }
+   
+// addition()
+
+
+
+
+// function addition(value1 , value2 = 0){
+//     // var value1 = +prompt("Enter num1")
+//     // var value2 = +prompt("Enter num2")
+
+//     console.log(value1 + " value1")
+//     console.log(value2 + " value2")
+//     console.log(value1 * value2 + " (Maths rules is Applying)")
+// }
+   
+// addition(3 , 10)
+
+
+
+// Calculator
+
+// function addition(value1  , opt, value2){
+    
+//     if(opt === "+"){
+//     console.log(value1 + value2 , "Addition")
+//     }else if(opt === "-"){
+//     console.log(value1 - value2 , "Subtraction")
+//     }else if(opt === "*"){
+//     console.log(value1 * value2 , "Multiplicaion")
+//     }else if(opt === "/"){
+//     console.log(value1 / value2 , "Division")
+//     }else if(opt === "%"){
+//         console.log(value1 % value2 , "Remainder")
+//     }else{
+//         console.log("Error!")
+//     }
+// }
+   
+//     var value1 = +prompt("Enter num1")
+//     var opt = prompt("Enter Opretor")
+//     var value2 = +prompt("Enter num2")
+//     addition(value1 , opt , value2)
+
+
+
+// function addition(value1 , value2 , opt){
+//     var value1 = +prompt("Enter num1")
+//     var value2 = +prompt("Enter num2")
+//     var opt = +prompt("Enter Opretor")
+
+
+//     // console.log(value1 + " value1")
+//     // console.log(value2 + " value2")
+//     console.log(value1 / value2 + " (Maths rules Applying)")
+// }
+// addition()
+
+// var arr = ["a", "b", "c"]
+// console.log(arr + " old array")
+
+// var copyArr = arr.slice(0)
+// console.log(copyArr + " new Array")
+
+
+
+
+// function fullName (FName , LName){
+
+//     return FName + " " +LName
+// }
+
+// var userName = fullName("Humayun" ,"Shaikh" )
+// console.log(userName)
+
+
+
+// var num = 220
+
+// var strNum = String(num)
+// var strNum = num.toString()
+// console.log(typeof num)
+// console.log(typeof strNum)
+
+
+// var str = "550.559"
+// // var num = parseInt(str)
+// var num = Number(str)
+// // console.log(typeof str + " "+ str)
+// console.log(typeof num + " " + num)
+
+// function greetUser(userName){
+//     // var userName = "humayun shaikh"
+//     // alert("hey ")
+//     return userName
+// }
+
+// var user = greetUser("Humayun Shaikh")
+// console.log(user)
+
+
+// function add (num1 = 0 , num2 = 0){
+//     return num1 + num2
+// }
+
+// var value = add(100, 800)
+// console.log(value)
+
+
+
+//mouseOver mouseOut practice
+
+// function mouseOver(){
+//     var input = document.getElementById("box1")
+//     console.log("checking" , input)
+//     box1.style.backgroundColor = "red";
+//     box1.style.borderRadius = "50%"
+// }
+
+// function mouseOut(){
+//     var input = document.getElementById("box1")
+//     box1.style.backgroundColor = "blue"
+//     box1.style.borderRadius = "0%"
+// }
+
+// inputFocus inputBlur practice
+
+// function inputFocus(){
+//     var input = document.getElementById("inpuT")
+//     // console.log(input)
+//     input.style.backgroundColor = "grey"
+// }
+
+// function inputBlur(){
+//     var input = document.getElementById("inpuT")
+//     // input.style.backgroundColor = "White"
+//     if(!input.value){
+//     input.style.backgroundColor = "red"
+//     input.style.border = "5px solid green"
+//     }else{
+//         input.style.backgroundColor = "White"
+//     }
+// }
+
+
+
+
+// setting and reading para
+
+// function zip(){
+//     var zipInput = document.getElementById("inputZip")
+//     var cityName = ""
+
+//     if(zipInput.value == ""){
+//         return
+//     }
+
+//     console.log(zipInput.value)
+
+
+//     if(zipInput.value == 111){
+//         cityName = "karachi"
+//     }else if (zipInput.value == 222){
+//         cityName = "lahore"
+//     }else if(zipInput.value == 333){
+//         cityName = "islamabad"
+//     }
+    
+//     console.log(cityName)
+
+//     var cityInput = document.getElementById("cityInput")
+//     cityInput.value = cityName
+// }
+
+
+
+
+// function getPara() {
+//     var para = document.getElementById("para")
+//     console.log(para.innerText)
+// }
+
+// function setPara() {
+//     var txt = "The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser"
+//     var para = document.getElementById("para")
+//     para.innerHTML += txt
+// }
+
+
+
+// var loremtxt = "Lorem ibsum dolor sit amet. consectetur adipisicing elit. Eveniet deserunt totam iste tempore excepturi quod est eum fuga earum aspernatur"
+// function seemore(parameter1){
+//     var para2 = document.getElementById("para2")
+//     var seeBtn = parameter1
+//     if (seeBtn.innerHTML == "see more"){
+//         seeBtn.innerHTML = "see less"
+//         para2.innerHTML = loremtxt
+//     }else{
+//         seeBtn.innerHTML = "see more"
+//         para2.innerHTML = "Lorem ibsum dolor sit amet."
+//     }
+// }
